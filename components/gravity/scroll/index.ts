4aae01ebf2d347938a0c45787b36d6438f9c4ef4
Shubham <shubhamjh175@gmail.com>
2026-06-19 19:57:41 +0530

    chore(scroll): barrel export for scroll toolkit

---
+++ components/gravity/scroll/index.ts
@@ -0,0 +1,5 @@
+export { Reveal } from "./reveal";
+export { Parallax } from "./parallax";
+export { WordReveal } from "./word-reveal";
+export { Pin } from "./pin";
+export { gsap, ScrollTrigger, prefersReducedMotion } from "./gsap";
